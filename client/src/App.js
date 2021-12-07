24c18ffd213aa7e7188cb03d9e8a62584711c692
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-12-07 16:50:55 +0100

    routing and starter code

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./Main/Main";
+import Login from "./Login/Login";
+import SignUp from "./SignUp/SignUp";
+import Header from "./Reusable/Header/Header";
+import Footer from "./Reusable/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
 
---
+++ client/src/Main/Main.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main-centered">
+        <div>
+          <h1>Develo P edia</h1>
+          <p>
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
