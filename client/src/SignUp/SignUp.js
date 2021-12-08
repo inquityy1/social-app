34a2aeaf2f840a29b4dbc57ceed2c9dddd134045
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-12-08 14:58:37 +0100

    signUp

--- client/src/SignUp/SignUp.js
+++ client/src/SignUp/SignUp.js
@@ -1,11 +1,23 @@
 import React from "react";
 
+import "./SignUp.css";
+
 function SignUp() {
   return (
     <div className="sign-up-container">
       <div className="sign-up">
-        <h1>SignUp</h1>
-        <p>Create your DeveloPEdia account</p>
+        <div className="sign-up-title">
+          <h1>Sign Up</h1>
+          <p>Create your DeveloPEdia account</p>
+        </div>
+
+        <form className="sign-up-form">
+          <input required type="text" placeholder="Name" />
+          <input required type="text" placeholder="Email Address" />
+          <input required type="password" placeholder="Password" />
+          <input required type="password" placeholder="Confirm Password" />
+          <button className="btn btn-primary">Submit</button>
+        </form>
       </div>
     </div>
   );
