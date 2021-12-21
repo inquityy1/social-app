13ecf8facf8883b26dd6a34ec2227ee0f0c87ec6
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-12-21 07:03:05 +0100

    add developers component

--- client/src/profiles/ProfileCard.js
+++ client/src/profiles/ProfileCard.js
@@ -11,23 +11,25 @@ function ProfileCard() {
         <p>Browse and connect with developer</p>
       </div>
 
-      <div className="card">
-        <div className="left-side">
-          <div className="icon">
-            <i class="fas fa-user-circle"></i>
-          </div>
+      <div className="user-info-container">
+        <div className="icon">
+          <i class="fas fa-user-circle"></i>
+        </div>
+        <div className="user-info">
           <div className="description">
             <h3>Nebojsa</h3>
             <p>Junior Web Developer</p>
-            <p>Vrbovac</p>
-            <Link to="/profiles/:name">View Profile</Link>
+            <p>in Vrbovac</p>
+            <Link className="btn btn-primary" to="/profiles/:name">
+              View Profile
+            </Link>
           </div>
-        </div>
-        <div className="right-card">
-          <h3>Skill Set:</h3>
-          <h3 className="skill">
-            <i class="fas fa-check"></i> React JS
-          </h3>
+          <div className="right-card">
+            <h3>Skill Set:</h3>
+            <h3 className="skill">
+              <i class="fas fa-check"></i> React JS
+            </h3>
+          </div>
         </div>
       </div>
     </div>
