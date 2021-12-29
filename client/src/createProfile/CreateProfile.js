380ff8bdaa078cf395fb1e9d1f579eb0061326a7
saibot777 <stefantrajkovic777@gmail.com>
2021-12-29 19:10:35 +0100

    Test rebase

--- client/src/createProfile/CreateProfile.js
+++ client/src/createProfile/CreateProfile.js
@@ -4,6 +4,7 @@ import CreateProfileForm from "./CreateProfileForm";
 import "./CreateProfile.css";
 
 export default function CreateProfile() {
+  console.log('ASSSAA')
   return (
     <div className="create-profile-container">
       <CreateProfileForm />
