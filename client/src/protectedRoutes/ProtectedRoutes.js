15f69fadf550f3ed24a31c7d2ed1a032615b2455
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-01-13 20:50:23 +0100

    Add quick fix

--- client/src/protectedRoutes/ProtectedRoutes.js
+++ client/src/protectedRoutes/ProtectedRoutes.js
@@ -2,9 +2,8 @@ import React from "react";
 import SignUp from "../signUp/SignUp";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoutes = ({ component: Component, ...rest }) => {
+const ProtectedRoutes = () => {
   const isAuthenticated = localStorage.getItem("jwtToken");
-  console.log("this", isAuthenticated);
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/register" />;
 };
