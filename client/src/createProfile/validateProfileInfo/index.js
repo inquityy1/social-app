fe913d562020f59f997c5bc0c1904184c22e0af2
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-01-19 15:32:32 +0100

    Add regex validation for website input

--- client/src/createProfile/validateProfileInfo/index.js
+++ client/src/createProfile/validateProfileInfo/index.js
@@ -1,23 +1,32 @@
 export default function validateProfileInfo(values) {
-    let errors = {};
-  
-    //handle
-    if (!values.handle) {
-      errors.handle = "Username/handle is required";
-    } else if (values.handle.length > 40) {
-      errors.handle = "Username cannot be more than 40 characters";
-    }
+  let errors = {};
 
-    //status
-    if (!values.status) {
-      errors.status = "Status is required";
-    }
-
-    //skills
-    if (!values.skills && values.skills.length === 0) {
-      errors.skills = "Skills is required";
-    }
-  
-    return errors;
+  //handle
+  if (!values.handle) {
+    errors.handle = "Username/handle is required";
+  } else if (values.handle.length > 40) {
+    errors.handle = "Username cannot be more than 40 characters";
   }
-  
+
+  //status
+  if (!values.status) {
+    errors.status = "Status is required";
+  }
+
+  //skills
+  if (!values.skills && values.skills.length === 0) {
+    errors.skills = "Skills is required";
+  }
+
+  //website
+  if (!values.website) {
+    errors.website = "Email required";
+  } else if (
+    !/^(https:\/\/www\.|http:\/\/www\.|www\.)[a-zA-Z0-9\-_$]+\.[a-zA-Z]{2,5}$/g.test(
+      values.website
+    )
+  ) {
+    errors.website = "You need .com at the end";
+  }
+  return errors;
+}
