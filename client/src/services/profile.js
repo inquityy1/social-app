3f09db2a0019e03f67fc5a2f741c7d83fece98a6
saibot777 <stefantrajkovic777@gmail.com>
2022-01-20 19:11:09 +0100

    Add service addExp

--- client/src/services/profile.js
+++ client/src/services/profile.js
@@ -5,6 +5,11 @@ const create = async (profileData) => {
   return axios.post(`${API_URL}/profiles/create`, profileData);
 };
 
+const saveExperience = async (expData) => {
+  return axios.post(`${API_URL}/profiles/add_experience`, expData)
+}
+
 export default {
   create,
+  saveExperience
 };
